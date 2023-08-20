9b8b89a232475b0c88e253f3f241f11c7f00452c
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-20 23:50:42 +0300

    fin

--- index.ts
+++ index.ts
@@ -2,16 +2,20 @@
 import { from } from 'rxjs';
 import { distinctUntilKeyChanged } from 'rxjs/operators';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/filtering/distinctuntilkeychanged
+// Example 1: Compare based on key
 // only output distinct values, based on the last emitted value
 const source$ = from([
   { name: 'Brian' },
-  { name: 'Joe' },
-  { name: 'Joe' },
+  { name: 'Brian' },
   { name: 'Sue' },
+  { name: 'Sue' },
+  { name: 'Leo+' },
+  { name: 'Leo+' },
+  { name: 'Joe' },
+  { name: 'Leo' },
 ]);
 
-source$
-  // custom compare based on name property
-  .pipe(distinctUntilKeyChanged('name'))
-  // output: { name: 'Brian }, { name: 'Joe' }, { name: 'Sue' }
-  .subscribe(console.log);
+// custom compare based on name property
+source$.pipe(distinctUntilKeyChanged('name')).subscribe(console.log);
+// output: { name: 'Brian }, { name: 'Joe' }, { name: 'Sue' }
